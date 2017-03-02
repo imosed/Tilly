457b1a22d43017db41b812f10833abddca6b107b
imosed <programming@jaredbell.name>
2017-03-01 21:20:46 -0500

    Changed setup procedure

---
+++ public/javascripts/precision.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.gran').keydown(function(e) {
+    if (e.which == 16) {
+      $(this).attr('step', '10');
+    }
+    if (e.which == 17) {
+      $(this).attr('step', '100');
+    }
+  });
+  $('.gran').keyup(function(e) {
+    if (e.which == 16 || e.which == 17) {
+      $(this).attr('step', '1');
+    }
+  });
+});
